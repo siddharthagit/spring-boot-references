bc5ad51efa6b76b4aee5bdb16c1bb11bcf59b654
Sidd B <siddb@bootng.com>
2019-03-05 20:25:42 -0800

    initial commit for spring boot thymeleaf

---
+++ spingboot-web-thymeleaf/src/main/java/com/blogspot/blogger/WebLoginController.java
@@ -0,0 +1,14 @@
+package com.blogspot.blogger;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebLoginController {
+
+  @GetMapping("/login")
+  public String login(Model model) {
+    return "login";
+  }
+}
