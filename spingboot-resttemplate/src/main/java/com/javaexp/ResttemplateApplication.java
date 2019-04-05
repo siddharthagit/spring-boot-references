52502e0fc807ac2286c3118c97e7fe7705c1888e
SIDDHARTHA BHATTACHARJEE <siddbhattacharjee@SIDDs-MacBook-Pro-P.local>
2019-04-05 11:04:34 -0700

    pom modify to execute as standalone jar

---
+++ spingboot-resttemplate/src/main/java/com/javaexp/ResttemplateApplication.java
@@ -0,0 +1,20 @@
+package com.javaexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@SpringBootApplication
+public class ResttemplateApplication {
+
+  private static final Logger log = LoggerFactory.getLogger(ResttemplateApplication.class);
+
+  public static void main(String args[]) {
+    log.info("about to call ResttemplateApplication.run()");
+    SpringApplication.run(ResttemplateApplication.class, args);
+    log.info("completed executing ResttemplateApplication.run()");
+  }
+
+}
