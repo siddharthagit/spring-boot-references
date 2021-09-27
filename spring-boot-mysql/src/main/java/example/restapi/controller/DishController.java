b35016ee5f46f886f3c3b6f02f3d250251b021c2
Sidd B <admin@bootng.com>
2021-09-26 20:18:13 -0700

    cleanup jersey dependencies

--- spring-boot-mysql/src/main/java/example/restapi/controller/DishController.java
+++ spring-boot-mysql/src/main/java/example/restapi/controller/DishController.java
@@ -4,7 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import javax.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,44 +13,26 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.bootng.service.DishService;
 
 import example.restapi.dto.CustomPageResponse;
 import example.restapi.model.Dish;
 import example.restapi.repository.DishRepository;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-//import org.springframework.hateoas.ResourceSupport;
-
 @RestController
-public class DishSBController {
+@RequestMapping("api/v1")
+public class DishController {
 
-	private static final Logger log = LoggerFactory.getLogger(DishSBController.class);
+	private static final Logger log = LoggerFactory.getLogger(DishController.class);
 
-	
-	//@Autowired
-	//DishService repo;
-	
 	@Autowired
 	DishRepository repo;
 	
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return name;
-	}
-	
-		
 	@GetMapping(value="/dishes",produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Dish> getDish() throws Exception {
 		log.info("-------------------------------");
--- spring-boot-mysql/src/main/java/example/restapi/controller/DishControllerV2.java
+++ spring-boot-mysql/src/main/java/example/restapi/controller/DishControllerV2.java
@@ -1,42 +1,23 @@
 package example.restapi.controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
-import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.bootng.service.DishService;
-
-import example.restapi.dto.CustomPageResponse;
 import example.restapi.model.Dish;
 import example.restapi.repository.DishRepository;
 
 import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -44,9 +25,9 @@ import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("api/v2")
-public class DishSBControllerV2 {
+public class DishControllerV2 {
 
-	private static final Logger log = LoggerFactory.getLogger(DishSBControllerV2.class);
+	private static final Logger log = LoggerFactory.getLogger(DishControllerV2.class);
 
 	@Autowired
 	DishRepository repo;
@@ -73,7 +54,6 @@ public class DishSBControllerV2 {
 	public List<Dish> csvToDishModel(InputStream is) throws IOException {
 		List<Dish> result = new ArrayList<>();
 
-		//CSVParser csvParser = new CSVParser(fileReader);
 		CSVFormat csvFormat = CSVFormat.EXCEL.withHeader();
 		InputStreamReader in = new InputStreamReader(is);
 		
